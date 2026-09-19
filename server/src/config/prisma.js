bfe04131cdc6ca9db7e919ed031d9785644131d1
devYuvraj2006 <justyuvraj2006@gmail.com>
2026-09-19 13:45:47 +0530

    Fix Prisma 7 production deployment

--- server/src/config/prisma.js
+++ server/src/config/prisma.js
@@ -1,10 +1,7 @@
 import 'dotenv/config';
 
-import pkg from '@prisma/client';
-
+import { PrismaClient } from '../generated/prisma/client.ts';
 import { PrismaPg } from '@prisma/adapter-pg';
-
-const { PrismaClient } = pkg;
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
